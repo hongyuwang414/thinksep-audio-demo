d97709f84351de2b347a3ec4b8532d0901b556bc
HONGYU.WANG <hongyuwang0414@gmail.com>
2026-07-23 03:39:46 +0800

    Add 14-trial MUSHRA listening test

---
+++ mushra/config.js
@@ -0,0 +1,5 @@
+window.MUSHRA_CONFIG = {
+  submitEndpoint: "https://baouqvhefvglfohfktmx.supabase.co/rest/v1/mushra_submissions",
+  publishableKey: "sb_publishable_imH2RciVviB3tcdX-t-bxQ_phT_QAcl",
+  studyId: "thinksep-mushra-v1"
+};
